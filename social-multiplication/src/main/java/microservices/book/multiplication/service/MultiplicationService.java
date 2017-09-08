55571cfab5a13b73d04c2edd05ba833a9045806c
mechero <moises.macero@gmail.com>
2017-09-08 16:08:09 +0200

    Commenting some code and adapting to new book title

--- social-multiplication/src/main/java/microservices/book/multiplication/service/MultiplicationService.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/service/MultiplicationService.java
@@ -4,6 +4,12 @@ import microservices.book.multiplication.domain.Multiplication;
 
 public interface MultiplicationService {
 
+    /**
+     * Creates a Multiplication object with two randomly-generated factors
+     * between 11 and 99.
+     *
+     * @return a Multiplication object with random factors
+     */
     Multiplication createRandomMultiplication();
 
 }
--- social-multiplication/src/main/java/microservices/book/multiplication/service/RandomGeneratorService.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/service/RandomGeneratorService.java
@@ -2,6 +2,9 @@ package microservices.book.multiplication.service;
 
 public interface RandomGeneratorService {
 
+    /**
+     * @return a randomly-generated factor. It's always a number between 11 and 99.
+     */
     int generateRandomFactor();
 
 }
